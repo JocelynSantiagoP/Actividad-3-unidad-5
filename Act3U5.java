6272df3d60cac350f6d045760491624fa1b6facf
JocelynSantiagoP <santipuenjocelyn546@gmail.com>
2025-10-20 16:25:46 -0700

    Add files via upload

---
+++ Act3U5.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.act3u5;
+import java.util.Scanner;
+/**
+ *
+ * @author USUARIO
+ */
+public class Act3U5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double[]dec = new double [10];
+        for (int i =0; i <=9; i++)
+            System.out.println("captura los numeros decimales");
+         dec [i] = sc.nextDouble();
+         sc.close();
+    }
+    public static void Regreso(Scanner sc){
+       double[]dec = new double [10];
+        for (int i =0; i<=9; i++)
+            System.out.println("captura los numeros decimales");
+         dec [i] = sc.nextDouble(); 
+    }
+    p
+}
