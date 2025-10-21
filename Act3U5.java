b904c30dbfbdbbfd362b20f01960a9f6808cac77
JocelynSantiagoP <santipuenjocelyn546@gmail.com>
2025-10-20 22:27:14 -0700

    Add files via upload

--- Act3U5.java
+++ Act3U5.java
@@ -14,7 +14,7 @@ public class Act3U5 {
         Scanner sc = new Scanner(System.in);
         double rs = Act3U5.decimales(0);
         mostrarContenido();
-        double resultado = promedio();
+        promedio();
     }
 
 
@@ -34,11 +34,14 @@ public class Act3U5 {
         System.out.println("1.1, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1, 10.2");
     }
     
-    public static double promedio (double prom){
+    public static void promedio (){
         double [] ar = {1.1, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1, 10.2};
-        double sum= ar[0]+ar[1]+ar[2]+ar[3]+ar[4]+ar[5]+ar[6]+ar[7]+ar[8]+ar[9];
-        prom =  (float) (sum/10);
-        return prom;
+        double sum = 0;
+        for (double elemento : ar){
+            sum+= elemento;
+        }
+        double prom = sum/10;
+        System.out.println(""+prom);
     } 
     
 
